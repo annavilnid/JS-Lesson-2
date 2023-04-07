2a299e40ca136f57991fce6842f6c43e145962e1
annavilnid <anna.vilnid@gmail.com>
2023-04-07 22:09:12 +0200

    ls6 04

--- src/lessons/lesson6/lesson6.ts
+++ src/lessons/lesson6/lesson6.ts
@@ -68,7 +68,6 @@ buyers.push(new Buyer('Brown', 'William', '789 Main St.',  '123456123458'))
 buyers.push(new Buyer('Johnson', 'Patricia','012 Main St.',  '321654321654'))
 
 let result = [...buyers].sort((a, b) => a.surname < b.surname ? -1 : 1 )
-console.log(buyers)
 console.log(result)
 
 // Класс Покупатель: Фамилия, Имя, Адрес, Номер банковского счета;
